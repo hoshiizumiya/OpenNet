e52b3c7ba23f79a0f700a835babf43d107c505f6
hoshiizumiya <Hoshiizumiya@outlook.com>
2026-09-26 18:36:27 +0800

    sync: Add DataTable HorizontalScrollBar https://github.com/lgztx96/CommunityToolkit.WinUI/pull/21
    [ci skip]

--- OpenNet/UI/Xaml/View/Pages/ServersPage.xaml.h
+++ OpenNet/UI/Xaml/View/Pages/ServersPage.xaml.h
@@ -1,18 +1,18 @@
-#pragma once
+﻿#pragma once
 
 #include "UI/Xaml/View/Pages/ServersPage.g.h"
 
 namespace winrt::OpenNet::UI::Xaml::View::Pages::implementation
 {
-    struct ServersPage : ServersPageT<ServersPage>
-    {
-        ServersPage();
-    };
+	struct ServersPage : ServersPageT<ServersPage>
+	{
+		ServersPage();
+	};
 }
 
 namespace winrt::OpenNet::UI::Xaml::View::Pages::factory_implementation
 {
-    struct ServersPage : ServersPageT<ServersPage, implementation::ServersPage>
-    {
-    };
+	struct ServersPage : ServersPageT<ServersPage, implementation::ServersPage>
+	{
+	};
 }
